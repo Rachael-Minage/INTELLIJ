72edb8a3fa57ae77f952f5a4e0b5eaa7cf6914a4
Rachael-Minage <lauryneminagge@gmail.com>
2022-02-12 16:05:49 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var nam : String = "rachael minage"
+    println(nam)
+    var age : Byte = 23
+    println(age)
+    var tel : Long = 254748655576
+    println(tel)
+    var weight : String = "53kg"
+    println(weight)
+    val ken : Boolean = true
+    println(ken)
+    val notken : Boolean = false
+    println(notken)
+
+
+
+
+}
